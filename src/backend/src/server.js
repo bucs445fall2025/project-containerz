592a26d6734b47b99b598dfe326777634e6f8c76
oliveiramnando <oliveiramnando@gmail.com>
2025-10-03 16:55:28 -0400

    CJS format; mounted authroute

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -1,23 +1,25 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+require('dotenv').config();;
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-import { connectDB } from './config/db.js';
-
-dotenv.config();
+const { connectDB } = require('./config/db');
+const authRoute = require('./routes/authRoute.js');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(cors({
   	origin: 'http://localhost:5173',
 	credentials: true, 
 }));
 
 app.get('/api/health', (_req, res) => res.json({ ok: true }));
+
+app.use('/api/auth', authRoute);
 
 const PORT = process.env.PORT || 3000;
 
