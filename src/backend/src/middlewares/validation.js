23b9d1efa0962f81b5e0d82bc505f4837da9bcc4
oliveiramnando <oliveiramnando@gmail.com>
2025-10-06 15:45:20 -0400

    schemas

--- src/backend/src/middlewares/validation.js
+++ src/backend/src/middlewares/validation.js
@@ -22,3 +22,30 @@ exports.signinSchema = Joi.object({
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     ))
 });
+
+exports.acceptCodeSchema = Joi.object({
+    email: Joi.string().min(6).max(60).required().email({
+        tlds: { allowed: ['com', 'net', 'edu'] }
+    }),
+    providedCode: Joi.number().required()
+});
+
+exports.changePasswordSchema = Joi.object({
+    newPassword: Joi.string().required().pattern(new RegExp(
+         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    )),
+    oldPassword: Joi.string().required().pattern(new RegExp(
+         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ))
+});
+
+exports.acceptFPCodeSchema = Joi.object({
+    email: Joi.string().min(6).max(60).required().email({
+        tlds: { allowed: ['com', 'net'] }
+    }),
+    providedCode: Joi.number().required(),
+    newPassword: Joi.string().required().pattern(new RegExp(
+         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ))
+});
+
