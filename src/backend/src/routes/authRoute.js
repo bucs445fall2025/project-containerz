f33c23dafdd7f02d21c4b418e4204b7a5666299b
oliveiramnando <oliveiramnando@gmail.com>
2025-10-06 16:03:10 -0400

    more auth routes

--- src/backend/src/routes/authRoute.js
+++ src/backend/src/routes/authRoute.js
@@ -12,5 +12,8 @@ router.post('/signout', identifier, authController.signout);
 router.patch('/send-verification-code', identifier, authController.sendVerificationCode);
 router.patch('/verify-verification-code', identifier, authController.verifyVerificationCode);
 
+router.patch('/change-password', identifier, authController.changePassword);
+// router.patch('/send-forgot-password-code', authController.sendForgotPasswordCode);
+// router.patch('/verify-forgot-password-code', authController.verifyForgotPasswordCode);
 
 module.exports = router;
