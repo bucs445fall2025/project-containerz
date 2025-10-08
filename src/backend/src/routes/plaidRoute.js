b32334c34caa9e113d76ab1020076e8d33b78ece
oliveiramnando <oliveiramnando@gmail.com>
2025-10-08 14:07:47 -0400

    transaction route

--- src/backend/src/routes/plaidRoute.js
+++ src/backend/src/routes/plaidRoute.js
@@ -1,16 +1,13 @@
 const router = require('express').Router();
 const { identifier } = require('../middlewares/identification.js');
-const {
-  createLinkToken,
-  exchangePublicToken,
-  getAccounts
-} = require('../controllers/plaidController.js');
+const plaidController = require('../controllers/plaidController.js');
 
 router.use(identifier);
 
-router.post('/link-token', createLinkToken);
-router.post('/set-access-token', exchangePublicToken);
-router.get('/accounts', getAccounts);
+router.post('/link-token', plaidController.createLinkToken);
+router.post('/set-access-token', plaidController.exchangePublicToken);
+router.get('/accounts', plaidController.getAccounts);
+router.get('/transactions', plaidController.getTransactions);
 
 module.exports = router;
 
