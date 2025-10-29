c75dc3ce31c86018409aaaaee485e97d40a8e374
oliveiramnando <oliveiramnando@gmail.com>
2025-10-28 23:27:37 -0400

    identifier

--- src/backend/src/routes/quantRoute.js
+++ src/backend/src/routes/quantRoute.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const quantController = require('../controllers/quantController.js');
+const { identifier } = require('../middlewares/identification.js');
+
+router.use(identifier);
 
 router.post('/mc/call', quantController.priceCallOption);
 router.post('/mc/asset', quantController.simAsset);
