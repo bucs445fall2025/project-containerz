efed408ea779151721835fa413f7b24e40af5a3e
oliveiramnando <oliveiramnando@gmail.com>
2025-11-04 18:40:21 -0500

    base for sim asset

--- src/pythonservice/app/main.py
+++ src/pythonservice/app/main.py
@@ -9,31 +9,19 @@ app = FastAPI(title='FinTool AI/Quant Service', version='0.1.0')
 def health():
     return {"ok": True}
 
-# @app.post('/simulate', response_model=MCResponse)
-# def simulate(req: MCRequest):
-#     price, stderr = price_european_call_spot_mc(
-#         S0=req.S0, K=req.K, T=req.T, r=req.r, sigma=req.sigma, n_paths=req.n_paths, seed=req.seed
-#     )
-#     return MCResponse(price=price, stderr=stderr)
 
 @app.post('/sim/asset', response_model=MCResponseAsset)
 def simulatePortfolio(req: MCRequestAsset):
-    portfolioFinalValues, meanFinalValue, stdFinalValue, expectedReturn, portfolioVar95, portfolioCvar95, params = gbm_portfolio(
-        assets=[a.model_dump() for a in req.assets], 
-        weights=req.weights, 
-        T=req.T, 
-        r=req.r,
-        n_steps=req.n_steps, 
-        n_paths=req.n_paths, 
-        seed=req.seed
+    FinalValue, meanFinalValue, stdFinalValue, expectedReturn, AssetVar95, AssetCvar95, params = gbm_portfolio(
+        asset=req.asset, 
     )
     return MCResponsePortfolio(
-        portfolioFinalValues=portfolioFinalValues if req.return_paths else None,
+        portfolioFinalValues=FinalValue,
         meanFinalValue=meanFinalValue,
         stdFinalValue=stdFinalValue,
         expectedReturn=expectedReturn,
-        portfolioVar95=portfolioVar95,
-        portfolioCvar95=portfolioCvar95,
+        AssetVar95=AssetVar95,
+        AssetCvar95=AssetCvar95,
         params=params
     )
 
