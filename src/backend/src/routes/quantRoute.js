fa14b62364706bf20e98187196573366f337d7d8
oliveiramnando <oliveiramnando@gmail.com>
2025-11-04 18:12:21 -0500

    route for getAssets

--- src/backend/src/routes/quantRoute.js
+++ src/backend/src/routes/quantRoute.js
@@ -4,8 +4,9 @@ const { identifier } = require('../middlewares/identification.js');
 
 router.use(identifier);
 
-router.post('/mc/call', quantController.priceCallOption);
+// router.post('/mc/call', quantController.priceCallOption);
 router.post('/mc/asset', quantController.simAsset);
 router.post('/mc/portfolio', quantController.simPortfolio);
+router.get('/holdingsAndSecurities', quantController.getHoldingsAndSecurities);
 
 module.exports = router;
