832de2dffb44d963de0b75f079960ea8fa75c84f
oliveiramnando <oliveiramnando@gmail.com>
2025-11-06 17:33:35 -0500

    financeScore route

--- src/backend/src/routes/quantRoute.js
+++ src/backend/src/routes/quantRoute.js
@@ -5,8 +5,10 @@ const { identifier } = require('../middlewares/identification.js');
 router.use(identifier);
 
 // router.post('/mc/call', quantController.priceCallOption);
+router.get('/holdingsAndSecurities', quantController.getHoldingsAndSecurities);
+router.post('/financeScore', quantController.financeScore);
 router.post('/mc/asset', quantController.simAsset);
 router.post('/mc/portfolio', quantController.simPortfolio);
-router.get('/holdingsAndSecurities', quantController.getHoldingsAndSecurities);
+
 
 module.exports = router;
